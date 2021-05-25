97909e4ac99c7b9bc117f7c20039a08de323d8e5
RomanShpylovyi <44481626+ShpylovyiRoman@users.noreply.github.com>
2021-05-25 22:01:55 +0300

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,11 +1,12 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import time as t
 
 def gen_signal(n, W_max, N):
     signals = [0] * N
     step = W_max/n
-
+    
     for i in range(n):
         A = random.random()
         fi = random.random()
@@ -22,7 +23,6 @@ def dft(signal):
         for k in range(0, N):
             spectrum[n] += signal[k] * np.exp(-2j * np.pi * k * n / N)
     return spectrum
-
 
 def fft(signal):
     N = len(signal)
@@ -45,34 +45,78 @@ def fft(signal):
         root = root * inverted_root
     return spectrum
 
+def interval(start, stop, step):
+    signals = []
+    N = []
 
-n = int(input("Numbers of garmonics = "))
-W_max = int(input("Border frequency = "))
+    for i in range(start, stop + 1, step):
+        signals.append(gen_signal(n, W_max, i))
+        N.append(i)
+
+    return [N, signals] 
+
+def get_time(interval, ft):
+    time = []
+    N = interval[0]
+    signal = interval[1]
+
+    for i in range(0, len(N)):
+        start_time = t.time()
+        ft(signal[i])
+        time.append(t.time() - start_time)
+
+    return [N, time]
+    
+    
+n = int(input("Harmonics = "))
+W_max = int(input("Frequency = "))
 N = int(input("Number of points = "))
-
+                            
 signal = gen_signal(n, W_max, N)
 
 spectrum_dft = dft(signal)
 spectrum_fft = fft(signal)
 spectrum_numpy_fft = np.fft.fft(signal)
 
-plt.figure(figsize=(10, 7))
+signal_interval = interval(100, 1500, 100)
+time_dft = get_time(signal_interval, dft)
+time_fft = get_time(signal_interval, fft)
+
+
+plt.figure(1, figsize=(10, 7))
 
 plt.subplot(2, 2, 1)
-plt.title('Our signal')
+plt.title('Signal')
 plt.plot(signal, 'k')
 
 plt.subplot(2, 2, 2)
-plt.title('Spectrum(discrete Fourier transform)')
+plt.title('Spectrum(dft)')
 plt.plot(np.abs(spectrum_dft), 'k')
 
 plt.subplot(2, 2, 3)
-plt.title('Spectrum(fft - fast Fourier transform)')
+plt.title('Spectrum(fft)')
 plt.plot(np.abs(spectrum_fft), 'k')
 
 plt.subplot(2, 2, 4)
 plt.title('Spectrum(numpy.fft)')
 plt.plot(np.abs(spectrum_numpy_fft), 'k')
+
+
+plt.figure(2, figsize=(10, 7))
+
+plt.subplot(3, 1, 1)
+plt.title('Time of dft')
+plt.plot(time_dft[0], time_dft[1], 'k')
+
+plt.subplot(3, 1, 2)
+plt.title('Time of fft')
+plt.plot(time_fft[0], time_fft[1], 'k')
+
+plt.subplot(3, 1, 3)
+plt.title('Comparison of dft and fft')
+plt.plot(time_dft[0], time_dft[1], label = "dft")
+plt.plot(time_fft[0], time_fft[1], label = "fft")
+plt.legend()
 
 plt.tight_layout()
 plt.show()
